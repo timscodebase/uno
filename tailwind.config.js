4863aeb9b744b65da0520274b186e5d2679f29de
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-09-20 12:04:52 -0700

    tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    colors: {
+      'blue': '#0000ff',
+      'red': '#ff0000',
+      'green': '#00ff00',
+      'yellow': '#ffff00',
+      'white': '#ffffff',
+      'black': '#000000',
+      'dark-green': '#228b22',
+      'light-gray': '#d2d2d2',
+      'light-grey': '#d2d2d2',
+      'dark-gray': '#333333',
+      'dark-grey': '#333333',
+    },
+    fontFamily: {
+      sans: ['Monoton', 'cursive'],
+      serif: ['Alfa Slab One', 'cursive'],
+    },
+    extend: {
+    }
   },
   plugins: [],
 }

f2f9f9a13e627ef74956f4e03eb0b676fc72d791
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-09-20 12:05:14 -0700

    Form setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,27 @@
 <script lang="ts">
-	import type { PageLoad } from './$types'
-	import { onMount } from 'svelte'
-	export let data: PageData
+	import type { PageData } from './$types'
 
-	const { unoDeck } = data
-
-	// Get the number of players from the form
-	let number_of_players_input: HTMLInputElement
-
-	onMount(() => {
-		number_of_players_input = document.querySelector(
-			'input#number_of_players'
-		) as HTMLInputElement
-		number_of_players_input.focus()
-	})
-
-	function get_number_of_players(): number {
-		return parseInt(number_of_players_input.value)
-	}
-
-	// Shuffle the Uno Deck
-	function shuffle(): void {
-		for (let i = unoDeck.length - 1; i > 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1))
-			;[unoDeck[i], unoDeck[j]] = [unoDeck[j], unoDeck[i]]
-		}
-	}
-
-	shuffle()
-	console.log('unoDeck: ', unoDeck)
-
-	function deal(): void {
-		const number_of_players = get_number_of_players()
-		// const cards_per_player = Math.floor(unoDeck.length / number_of_players)
-		const cards_per_player = 7
-		const cards_in_play = number_of_players * cards_per_player
-		const remainder = unoDeck.length % cards_in_play
-
-		const players = []
-		for (let i = 0; i < number_of_players; i++) {
-			const player = []
-			for (let j = 0; j < cards_per_player; j++) {
-				player.push(unoDeck.pop())
-			}
-			players.push(player)
-		}
-
-		// Deal the remainder
-		for (let i = 0; i < remainder; i++) {
-			players[i].push(unoDeck.pop())
-		}
-
-		console.log(players)
-	}
+	export const data: PageData = {}
+	export const form: FormData = new FormData()
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<h1 class="text-6xl font-bold">Hello world!</h1>
 
-<form method="POST" on:submit|preventDefault={deal}>
-	<div>
-		<label for="number_of_players" />
-		<input
-			type="number"
-			name="number_of_players"
-			id="number_of_players"
-			min="2"
-			max="10"
-			value="2"
-		/>
-		<button type="submit">Deal</button>
-	</div>
+<form action="?/deal" method="POST">
+	<label for="number_of_players" />
+	<input
+		type="number"
+		name="number_of_players"
+		id="number_of_players"
+		min="2"
+		max="10"
+		value="2"
+	/>
+	<button class="outline px-5 py-2">DEAL</button>
 </form>
 
 <style lang="postcss">
 	:global(html) {
-		background-color: theme(colors.gray.100);
+		background-color: theme(colors.dark-green);
 	}
 </style>

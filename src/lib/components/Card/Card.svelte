b751879b265caa0c3287efadc246f891cb19eddc
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-09-20 22:02:42 -0700

    Card Start

---
+++ src/lib/components/Card/Card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { UnoColorType, UnoValueType } from '$lib'
+
+	export let color: UnoColorType
+	export let value: UnoValueType
+	export let flipped: boolean = false
+</script>
+
+{#if !flipped}
+	<div class="card_back" />
+{:else}
+	<div class="card_back" />
+{/if}
+
+<style>
+</style>

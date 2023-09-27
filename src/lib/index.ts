4731ef989f46c25e87fc22178694dc0bfb062b8d
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-09-26 22:19:53 -0700

    Oups

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -29,7 +29,7 @@ export { H1 }
 import { shuffle } from './cards'
 export { shuffle }
 
-import { TailwindCSS } from './components/TailwindCSS'
+import TailwindCSS from './components/TailwindCSS'
 export { TailwindCSS }
 
 import UnoCard from './components/UnoCard'

1f0f81e937e7109304f69d44504c9416c2e3bd9d
Yash Thipsay <yashthpsay@gmail.com>
2023-09-27 07:34:36 +0530

    documentation of changes

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 	export let type: 'button' | 'submit' | 'reset' = 'button'
 </script>
 
@@ -10,26 +9,25 @@
 	<slot />
 </button>
 
-
 <style>
 	/* Adding tailwind classes for being used in styled components */
 	@tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-
+	@tailwind components;
+	@tailwind utilities;
 
 	@layer utilities {
-  .shadow-effect {
-    box-shadow: -1px 1px 2px #000000, -3px 3px 2px #000000,
-      -5px 5px 2px #000000, -7px 7px 2px #000000, -9px 9px 2px #000000,
-      -11px 11px 2px #000000, -13px 13px 2px #000000;
-    transition: box-shadow 0.1s;
-  }
+		/* Apply shadow-effect utility classes */
+		.shadow-effect {
+			box-shadow: -1px 1px 2px #000000, -3px 3px 2px #000000,
+				-5px 5px 2px #000000, -7px 7px 2px #000000, -9px 9px 2px #000000,
+				-11px 11px 2px #000000, -13px 13px 2px #000000;
+			transition: box-shadow 0.1s;
+		}
 
-  .shadow-effect:hover {
-    transform: translate(-3px, 3px);
-	box-shadow: -1px 1px 2px #000000, -3px 3px 2px #000000,
-			-5px 5px 2px #000000, -7px 7px 2px #000000, -9px 9px 2px #000000;
-  }
-}
+		.shadow-effect:hover {
+			transform: translate(-3px, 3px);
+			box-shadow: -1px 1px 2px #000000, -3px 3px 2px #000000,
+				-5px 5px 2px #000000, -7px 7px 2px #000000, -9px 9px 2px #000000;
+		}
+	}
 </style>
